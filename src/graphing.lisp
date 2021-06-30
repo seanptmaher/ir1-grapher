67d2da369819b458bb20deb2825c69196917bbec
Sean Maher <seanptmaher@gmail.com>
2021-06-30 17:28:53 +0000

    add interactive graphing capability

--- src/graphing.lisp
+++ src/graphing.lisp
@@ -1,17 +1,26 @@
 (in-package :ir1-grapher)
 
+(defun make-graph ()
+  (make-instance 'graph
+                 :table (make-hash-table :test 'eq :size 63)
+                 :stream (make-string-output-stream)
+                 :bfs-table (make-hash-table :test 'eq :size 63)
+                 :obj-table (make-hash-table :test 'eq :size 63)
+                 :codename-table (make-hash-table :test 'equal :size 63)
+                 :codename-number 0))
+
 (defmacro with-digraph ((graph-sym) &body body)
-  `(let ((,graph-sym (make-instance 'graph
-                                    :table (make-hash-table :test 'eq :size 63)
-                                    :stream (make-string-output-stream))))
+  `(let ((,graph-sym (make-graph)))
      (progn
        (write-string (format nil "digraph {~%") (stream ,graph-sym))
        ,@body
        (write-string "}" (stream ,graph-sym))
-       (get-output-stream-string (stream ,graph-sym)))))
+       (values (get-output-stream-string (stream ,graph-sym))
+               ,graph-sym))))
 
 (eval-when (:compile-toplevel :load-toplevel)
   (defparameter *subgraph-number* 0))
+
 (defmacro with-subgraph ((graph-sym &optional label) &body body)
   `(progn (write-string ,(if label
                             `(format nil "subgraph ~A_~A {~%"
@@ -28,9 +37,16 @@
 
 (defparameter *test-component* nil)
 
+;; table: obj -> bool
+;; obj-table: obj -> codename
+;; codename-table: codename -> obj
 (defclass graph ()
-  ((stream :initarg :stream :reader stream)
-   (table :initarg :table :reader table)))
+  ((stream :initarg :stream :accessor stream)
+   (table :initarg :table :accessor table)
+   (bfs-table :initarg :bfs-table :reader bfs-table)
+   (obj-table :initarg :obj-table :reader obj-table)
+   (codename-table :initarg :codename-table :reader codename-table)
+   (codename-number :initarg :codename-number :accessor codename-number)))
 
 (defmethod graph (object &optional (distance -1))
   (with-digraph (g)
@@ -79,8 +95,14 @@
 
 (defmethod edge ((graph graph) from to &rest options)
   (assert (= 0 (mod (length options) 2)))
-  (format (stream graph) "\"~A\" -> \"~A\"[~A];~%"
+  (format (stream graph) "\"{~A} ~A\" -> \"{~A} ~A\"[~A];~%"
+          (if (gethash from (obj-table graph))
+              (gethash from (obj-table graph))
+              (add-to-code-tables graph from))
           (cl-ppcre:regex-replace-all "\\\\" (cl-ppcre:regex-replace-all "\"" (clamp (display from)) "'") "\\\\\\\\")
+          (if (gethash to (obj-table graph))
+              (gethash to (obj-table graph))
+              (add-to-code-tables graph to))
           (cl-ppcre:regex-replace-all "\\\\" (cl-ppcre:regex-replace-all "\"" (clamp (display to)) "'") "\\\\\\\\")
           ;; (cl-ppcre:regex-replace-all "\"" (clamp (display to)) "'")
           (apply #'concatenate
@@ -102,16 +124,6 @@
                        (unless (find-if (lambda (x) (string= x "color")) options)
                          '("color" "blue")))))
 
-;; ;; same thing here with REFs
-;; (defmethod edge ((graph graph) from (to sb-c::ref) &rest options)
-;;   (apply #'edge (nconc (list graph from (sb-c::ref-leaf to))
-;;                        (modify-str-plist options "label"
-;;                                          (lambda (x)
-;;                                            (concatenate 'string x
-;;                                                         (format nil "[ref: %source-name: ~A]"
-;;                                                                 (sb-c::ref-%source-name to)))))
-;;                        (unless (find-if (lambda (x) (string= x "color")) options)
-;;                          '("color" "yellow")))))
 
 (defmethod edge ((graph graph) from (to list) &rest options)
   ;; (when (null to)
@@ -158,12 +170,6 @@
   (format nil "BIND [~A]"
           (sxhash bind)))
 
-;;; this is outdated code from when I wasn't displaying refs. If it is
-;;; truly the case that refs need to be displayed, then this
-;;; could be deleted
-
-;; (defmethod display ((ref sb-c::ref))
-;;   (error "trying to display a REF, this shouldn't happen"))
 (defmethod display ((ref sb-c::ref))
   (format nil "REF [~A]:~%derived-type: ~A"
           (sxhash ref)
@@ -199,113 +205,242 @@
 (defmethod display (obj)
   (format nil "NOT SUPPORTED YET:~% ~A" obj))
 
-(defmacro edge-and-add (graph distance from to &rest options)
-  (let ((g (gensym))
-        (tg (gensym))
-        (d (gensym)))
-    `(let ((,g ,graph)
-           (,tg ,to)
-           (,d ,distance))
-       (if (= ,d 0)
-           (edge ,g ,from ,tg ,@options)
-           (prog2
-               (edge ,g ,from ,tg ,@options)
-               (add ,g ,tg (if (> ,d 0)
-                               (- ,d 1)
-                               ,d)))))))
+;; New way to do this is to have two tables:
+
+;; rec-table -> contains all the nodes from which to recurse
+;; codenames -> goes from randomly generated bite to nodes in order to interactively expand
 
 ;; this creates the wrapper used inside the add methods to only run
 ;; the edge-adding/recursing code if we've already seen this object
-(defmacro unig ((graph obj) &body body)
+(defmacro unig ((table obj) &body body)
+  (let ((tab (gensym))
+        (o (gensym)))
+    `(let ((,tab ,table)
+           (,o ,obj))
+       (unless (nth-value 1 (gethash ,o ,tab))
+         (setf (gethash ,o ,tab) t)
+         ,@body))))
+
+;; returns (values graph codeword-table)
+(defmethod render-graph (graph)
+  (get-output-stream-string (stream graph))
+  (setf (table graph) (make-hash-table :test 'eq :size 63))
+  (write-string (format nil "digraph {~%") (stream graph))
+  (maphash #'(lambda (k v) (declare (ignore v)) (edges graph k)) (bfs-table graph))
+  (write-string "}" (stream graph))
+  (get-output-stream-string (stream graph)))
+
+(defmethod expand-graph-codename (graph codename)
+  (setf (gethash (gethash codename (codename-table graph))
+                 (bfs-table graph))
+        t))
+
+(defun add-to-code-tables (graph object)
+  (if (nth-value 1 (gethash object (obj-table graph)))
+      (gethash object (obj-table graph))
+      (let ((new-codename (new-codename graph)))
+        (setf (gethash object (obj-table graph))
+              new-codename
+              (gethash new-codename (codename-table graph))
+              object)
+        new-codename)))
+
+(defun new-codename (graph)
+  (let ((res (format nil "~X" (codename-number graph))))
+    (incf (codename-number graph))
+    res))
+
+(let ((curr-graph nil))
+  (defun interactively-graph (graph)
+    (setf curr-graph graph))
+  (defun output (&optional (file nil))
+    (if file
+        (prog2 (Format t "ah") (save-graph (render-graph curr-graph) file))
+        (render-graph curr-graph)))
+  (defun expand (codename)
+    (setf (gethash (gethash codename (codename-table curr-graph))
+                   (bfs-table curr-graph))
+          t)))
+
+;; need way to simply output edges, adding is not done.
+
+(defmethod edges ((graph graph) (objects list))
+  (mapc #'(lambda (o) (edges graph o)) objects))
+
+;; a component should be rendered as a subgraph
+(defmethod edges ((graph graph) (component sb-c::component))
+  (unig ((table graph) component)
+    (edge graph component (sb-c::component-head component) "label" "head")
+    (edge graph component (sb-c::component-tail component) "label" "tail")))
+
+(defmethod edges ((graph graph) (cblock sb-c::cblock))
+  (unig ((table graph) cblock)
+    (edge graph cblock (sb-c::block-component cblock) "label" "component")
+    (edge graph cblock (sb-c::block-succ cblock) "label" "succ")
+    (edge graph cblock (sb-c::block-pred cblock) "label" "pred")
+    (edge graph cblock (sb-c::block-start cblock) "label" "start")))
+
+;; Unfinished
+(defmethod edges ((graph graph) (cl sb-c::clambda))
+  (unig ((table graph) cl)
+    (edge graph cl (sb-c::lambda-home cl) "label" "home")
+    (edge graph cl (sb-c::lambda-vars cl) "label" "vars")))
+
+(defmethod edges ((graph graph) (cr sb-c::creturn))
+  (unig ((table graph) cr)
+    (edge graph cr (sb-c::return-lambda cr) "label" "lambda")
+    (edge graph cr (sb-c::return-result cr) "label" "result")))
+
+;; (defmethod edges ((graph graph) (node sb-c::node))
+;;   )
+
+;; this is just a dummy function to skip the CTRAN
+(defmethod edges ((graph graph) (ct sb-c::ctran))
+  (unig ((table graph) ct)
+    ))
+
+(defmethod edges ((graph graph) (ref sb-c::ref))
+  (unig ((table graph) ref)
+    (edge graph ref (sb-c::ref-leaf ref) "label" "leaf")
+    (edge graph ref (sb-c::ref-next ref) "label" "next")
+    (edge graph ref (sb-c::ref-lvar ref) "label" "lvar")))
+
+(defmethod edges ((graph graph) (bind sb-c::bind))
+  (unig ((table graph) bind)
+    (edge graph bind (sb-c::bind-lambda bind) "label" "lambda")
+    (edge graph bind (sb-c::bind-next bind) "label" "next" "color" "green")
+    (edge graph bind (sb-c::bind-prev bind) "label" "prev" "color" "red")))
+
+(defmethod edges ((graph graph) (comb sb-c::combination))
+  (unig ((table graph) comb)
+    (edge graph comb (sb-c::combination-fun comb) "label" "fun")
+    (edge graph comb (sb-c::combination-args comb) "label" "args")))
+
+(defmethod edges ((graph graph) (lvar sb-c::lvar))
+  (unig ((table graph) lvar)
+    (edge graph lvar (sb-c::lvar-dest lvar) "label" "dest" "color" "brown")
+    (edge graph lvar (sb-c::lvar-uses lvar) "label" "uses")))
+
+(defmethod edges ((graph graph) (lamvar sb-c::lambda-var))
+  (unig ((table graph) lamvar)
+    (edge graph lamvar (sb-c::lambda-var-home lamvar) "label" "home")
+    (edge graph lamvar (sb-c::lambda-var-sets lamvar) "label" "sets")))
+
+(defmethod edges ((graph graph) (entry sb-c::entry))
+  (unig ((table graph) entry)
+    (edge graph entry (sb-c::entry-exits entry) "label" "exits")
+    (edge graph entry (sb-c::entry-cleanup entry) "label" "cleanup")
+    (edge graph entry (sb-c::entry-next entry) "label" "next")
+    (edge graph entry (sb-c::entry-prev entry) "label" "prev")))
+
+(defmethod edges ((graph graph) object)
+  (unig ((table graph) object)))
+
+(defmacro bfs-unig ((graph obj) &body body)
   (let ((g (gensym))
         (o (gensym)))
     `(let ((,g ,graph)
            (,o ,obj))
-       (unless (in-graph ,g ,o)
-         (table-add ,g ,o)
-         ,@body))))
+       (unless (nth-value 1 (gethash ,o (bfs-table ,g)))
+         (setf (gethash ,o (bfs-table ,g)) t)
+         ,@body)))
+  )
 
-(defmethod add ((graph graph) (objects list) (distance integer))
-  (mapc #'(lambda (o) (add graph o distance)) objects))
+(defmethod bfs-add ((graph graph) (distance integer) (objects list))
+  (mapc #'(lambda (o) (bfs-add graph distance o)) objects))
 
 ;; a component should be rendered as a subgraph
-(defmethod add ((graph graph) (component sb-c::component) (distance integer))
-  (unig (graph component)
-    (with-subgraph (graph "component")
-      (edge-and-add graph distance component (sb-c::component-head component) "label" "head")
-      (edge-and-add graph distance component (sb-c::component-tail component) "label" "tail"))))
+(defmethod bfs-add ((graph graph) (distance integer) (component sb-c::component))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph component)
+      (bfs-add graph distance (sb-c::component-head component))
+      (bfs-add graph distance (sb-c::component-tail component)))))
 
-(defmethod add ((graph graph) (cblock sb-c::cblock) (distance integer))
-  (unig (graph cblock)
-    (edge-and-add graph distance cblock (sb-c::block-component cblock) "label" "component")
-    (edge-and-add graph distance cblock (sb-c::block-succ cblock) "label" "succ")
-    (edge-and-add graph distance cblock (sb-c::block-pred cblock) "label" "pred")
-    (edge-and-add graph distance cblock (sb-c::block-start cblock) "label" "start")))
+(defmethod bfs-add ((graph graph) (distance integer) (cblock sb-c::cblock))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph cblock)
+      (bfs-add graph distance (sb-c::block-component cblock))
+      (bfs-add graph distance (sb-c::block-succ cblock))
+      (bfs-add graph distance (sb-c::block-pred cblock))
+      (bfs-add graph distance (sb-c::block-start cblock)))))
 
 ;; Unfinished
-(defmethod add ((graph graph) (cl sb-c::clambda) (distance integer))
-  (unig (graph cl)
-    (edge-and-add graph distance cl (sb-c::lambda-home cl) "label" "home")
-    (edge-and-add graph distance cl (sb-c::lambda-vars cl) "label" "vars")
+(defmethod bfs-add ((graph graph) (distance integer) (cl sb-c::clambda))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph cl)
+      (bfs-add graph distance (sb-c::lambda-home cl))
+      (bfs-add graph distance (sb-c::lambda-vars cl)))
     )
   )
 
-(defmethod add ((graph graph) (cr sb-c::creturn) (distance integer))
-  (unig (graph cr)
-    (edge-and-add graph distance cr (sb-c::return-lambda cr) "label" "lambda")
-    (edge-and-add graph distance cr (sb-c::return-result cr) "label" "result")))
+(defmethod bfs-add ((graph graph) (distance integer) (cr sb-c::creturn))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph cr)
+      (bfs-add graph distance (sb-c::return-lambda cr))
+      (bfs-add graph distance (sb-c::return-result cr)))))
 
-;; (defmethod add ((graph graph) (node sb-c::node) (distance integer))
+;; (defmethod bfs-add ((graph graph) (distance integer) (node sb-c::node))
 ;;   )
 
 ;; this is just a dummy function to skip the CTRAN
-(defmethod add ((graph graph) (ct sb-c::ctran) (distance integer))
-  (unig (graph ct)
-    (add graph (sb-c::ctran-next ct) distance)))
+(defmethod bfs-add ((graph graph) (distance integer) (ct sb-c::ctran))
+  (when (> distance 0)
+    (bfs-unig (graph ct)
+      (bfs-add graph distance (sb-c::ctran-next ct)))))
 
-(defmethod add ((graph graph) (ref sb-c::ref) (distance integer))
-  (unig (graph ref)
-    (edge-and-add graph distance ref (sb-c::ref-leaf ref) "label" "leaf")
-    (edge-and-add graph distance ref (sb-c::ref-next ref) "label" "next")
-    (edge-and-add graph distance ref (sb-c::ref-lvar ref) "label" "lvar")))
+(defmethod bfs-add ((graph graph) (distance integer) (ref sb-c::ref))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph ref)
+      (bfs-add graph distance (sb-c::ref-leaf ref))
+      (bfs-add graph distance (sb-c::ref-next ref))
+      (bfs-add graph distance (sb-c::ref-lvar ref)))))
 
+(defmethod bfs-add ((graph graph) (distance integer) (bind sb-c::bind))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph bind)
+      (bfs-add graph distance (sb-c::bind-lambda bind))
+      (bfs-add graph distance (sb-c::bind-next bind))
+      (bfs-add graph distance (sb-c::bind-prev bind)))))
 
-(defmethod add ((graph graph) (bind sb-c::bind) (distance integer))
-  (unig (graph bind)
-    (edge-and-add graph distance bind (sb-c::bind-lambda bind) "label" "lambda")
-    (edge-and-add graph distance bind (sb-c::bind-next bind) "label" "next" "color" "green")
-    (edge-and-add graph distance bind (sb-c::bind-prev bind) "label" "prev" "color" "red")))
+(defmethod bfs-add ((graph graph) (distance integer) (comb sb-c::combination))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph comb)
+      (bfs-add graph distance (sb-c::combination-fun comb))
+      (bfs-add graph distance (sb-c::combination-args comb)))))
 
-(defmethod add ((graph graph) (comb sb-c::combination) (distance integer))
-  (unig (graph comb)
-    (edge-and-add graph distance comb (sb-c::combination-fun comb) "label" "fun")
-    (edge-and-add graph distance comb (sb-c::combination-args comb) "label" "args")))
+(defmethod bfs-add ((graph graph) (distance integer) (lvar sb-c::lvar))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph lvar)
+      (bfs-add graph distance (sb-c::lvar-dest lvar))
+      (bfs-add graph distance (sb-c::lvar-uses lvar)))))
 
-(defmethod add ((graph graph) (lvar sb-c::lvar) (distance integer))
-  (unig (graph lvar)
-    (edge-and-add graph distance lvar (sb-c::lvar-dest lvar) "label" "dest" "color" "brown")
-    (edge-and-add graph distance lvar (sb-c::lvar-uses lvar) "label" "uses")))
-
-(defmethod add ((graph graph) (lamvar sb-c::lambda-var) (distance integer))
-  (unig (graph lamvar)
-    (edge-and-add graph distance lamvar (sb-c::lambda-var-home lamvar) "label" "home")
-    (edge-and-add graph distance lamvar (sb-c::lambda-var-sets lamvar) "label" "sets")
+(defmethod bfs-add ((graph graph) (distance integer) (lamvar sb-c::lambda-var))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph lamvar)
+      (bfs-add graph distance (sb-c::lambda-var-home lamvar))
+      (bfs-add graph distance (sb-c::lambda-var-sets lamvar)))
     ))
 
-(defmethod add ((graph graph) (entry sb-c::entry) (distance integer))
-  (unig (graph entry)
-    (edge-and-add graph distance entry (sb-c::entry-exits entry) "label" "exits")
-    (edge-and-add graph distance entry (sb-c::entry-cleanup entry) "label" "cleanup")
-    (edge-and-add graph distance entry (sb-c::entry-next entry) "label" "next")
-    (edge-and-add graph distance entry (sb-c::entry-prev entry) "label" "prev"))
+(defmethod bfs-add ((graph graph) (distance integer) (entry sb-c::entry))
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph entry)
+      (bfs-add graph distance (sb-c::entry-exits entry))
+      (bfs-add graph distance (sb-c::entry-cleanup entry))
+      (bfs-add graph distance (sb-c::entry-next entry))
+      (bfs-add graph distance (sb-c::entry-prev entry))))
   )
 
-;; I don't know whether it would be useful to have any information
-;; about constant values, but it can be added in here pretty easily
-(defmethod add ((graph graph) (const sb-c::constant) (distance integer))
-  (declare (ignorable distance))
-  (unig (graph const)))
-
-(defmethod add ((graph graph) object (distance integer))
-  (declare (ignorable object distance))
-  (unig (graph object)))
+(defmethod bfs-add ((graph graph) (distance integer) object)
+  (when (> distance 0)
+    (decf distance)
+    (bfs-unig (graph object))))
